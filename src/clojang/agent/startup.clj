356ba435f1d51c3c6f423706e172699e3063638d
Duncan McGreggor <oubiwann@gmail.com>
2016-05-22 18:00:35 -0500

    Updated logging and used new const namespace.

--- src/clojang/agent/startup.clj
+++ src/clojang/agent/startup.clj
@@ -1,36 +1,35 @@
 (ns clojang.agent.startup
   ""
-  (:require [clojure.string :as string]
-            [clojure.tools.logging :as log]
+  (:require [clojure.tools.logging :as log]
             [dire.core :refer [with-handler!]]
+            [clojusc.twig :as logger]
             [jiface.otp.nodes :as nodes]
             [jiface.util :as util]
-            [twig.core :as twig])
-  (:import [java.lang.instrument])
+            [clojang.agent.const :as const])
+  (:import [java.lang.instrument]
+           [java.awt HeadlessException SplashScreen])
   (:gen-class
     :methods [^:static [premain [String java.lang.instrument.Instrumentation] void]]))
 
 (defn get-node-name
   "Get the node name."
   []
-  (let [short-name (System/getProperty "node.sname")
-        long-name (System/getProperty "node.name")]
-    (format "%s@%s"
-            (if-not (nil? short-name)
-              short-name
-              long-name)
-            (util/get-hostname))))
+  (format "%s@%s"
+    (if-not (nil? const/short-name)
+      const/short-name
+      const/long-name)
+    (util/get-hostname)))
 
 (defn perform-gui-tasks
   "Close the custom splash screen."
   []
-  (if-let [screen (java.awt.SplashScreen/getSplashScreen)]
+  (if-let [screen (SplashScreen/getSplashScreen)]
     (.close screen)))
 
 (defn perform-node-tasks
   "This is the function that sets up a running node for a given JVM."
   []
-  (twig/set-level! 'clojang :info)
+  (logger/set-level! 'clojang :info)
   (let [node-name (get-node-name)]
     (log/infof "Bringing up OTP node on %s ..." node-name)
     (nodes/node node-name)))
@@ -38,9 +37,9 @@
 (defn headless?
   "Check to see if this JVM is declared as being headless."
   []
-  (if (nil? (System/getProperty "headless"))
-    true
-    false))
+  (if (nil? const/headless)
+    false
+    true))
 
 (defn -premain
   [args instrument]
@@ -49,6 +48,6 @@
     (perform-gui-tasks)))
 
 (with-handler! #'perform-gui-tasks
-  java.awt.HeadlessException
+  HeadlessException
   (fn [e & args]
-    (log/warn (string/trim (.getMessage e)))))
+    (log/warn (.getMessage e))))
