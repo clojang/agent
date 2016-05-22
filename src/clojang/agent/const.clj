6ad036b3b42b9c3614fb28187bfe644d9ad8929e
Duncan McGreggor <oubiwann@gmail.com>
2016-05-22 15:42:36 -0500

    Added constants.

---
+++ src/clojang/agent/const.clj
@@ -0,0 +1,6 @@
+(ns clojang.agent.const
+  "Clojang agent constants")
+
+(def short-name (System/getProperty "node.sname"))
+(def long-name (System/getProperty "node.name"))
+(def headless (System/getProperty "headless"))
