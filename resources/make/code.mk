d9962215d9d26fb41c38e69173451185d911fad5
Duncan McGreggor <oubiwann@gmail.com>
2016-05-22 14:31:40 -0500

    Updated target.

--- resources/make/code.mk
+++ resources/make/code.mk
@@ -4,7 +4,6 @@ compile:
 show-versions:
 	@echo Project: $(PROJ), $(PROJ_VERSION)
 	@echo Erlang: $(ERL_VERSION)
-	@echo JInterface: $(JINTERFACE_VER)
 	@echo Clojure: $(CLOJURE_VER)
 	@echo lein/JVM: $(shell lein version)
 
